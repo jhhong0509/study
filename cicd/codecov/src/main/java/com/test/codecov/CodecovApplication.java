f3e02fe2e79be5c96cbaeb16318f3183c50535a4
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-06-03 15:36:26 +0900

    [INITIAL] codecov

---
+++ cicd/codecov/src/main/java/com/test/codecov/CodecovApplication.java
@@ -0,0 +1,13 @@
+package com.test.codecov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodecovApplication.class, args);
+	}
+
+}
