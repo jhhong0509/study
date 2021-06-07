8f875f357f1fb87b80376960e6d9085c6dfc44b2
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-06-07 16:55:54 +0900

    [FIX] entity

--- mentoring/secretmanager/src/main/java/com/mentoring/secretmanager/test/Test.java
+++ mentoring/secretmanager/src/main/java/com/mentoring/secretmanager/test/Test.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Getter
@@ -16,7 +17,7 @@ import javax.persistence.Id;
 @Entity
 public class Test {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
