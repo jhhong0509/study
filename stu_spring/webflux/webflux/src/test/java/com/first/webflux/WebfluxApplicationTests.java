0650442cf2d0fd32ddeacfb287939011a74e2937
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-06-07 23:26:20 +0900

    INITIAL COMMIT

---
+++ stu_spring/webflux/webflux/src/test/java/com/first/webflux/WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
