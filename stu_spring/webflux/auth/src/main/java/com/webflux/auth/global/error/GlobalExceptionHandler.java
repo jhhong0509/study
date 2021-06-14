f5e1d9da93efbc5f071de41ddfe115e05e3ce2c1
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-06-14 11:24:02 +0900

    :bookmark_tabs: :: (#6) Exception Handler 추가

---
+++ stu_spring/webflux/auth/src/main/java/com/webflux/auth/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.webflux.auth.global.error;
+
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.*;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+@Order(-2)
+@Component
+public class GlobalExceptionHandler extends AbstractErrorWebExceptionHandler {
+    public GlobalExceptionHandler(GlobalErrorAttributes errorAttributes,
+                                  ApplicationContext applicationContext, ServerCodecConfigurer configurer) {
+        super(errorAttributes, new WebProperties.Resources(), applicationContext);
+        super.setMessageWriters(configurer.getWriters());
+    }
+
+    @Override
+    protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
+        return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse);
+    }
+
+    private Mono<ServerResponse> renderErrorResponse(ServerRequest request) {
+        Map<String, Object> errorPropertiesMap = getErrorAttributes(request, ErrorAttributeOptions.defaults());
+        int status = Integer.parseInt(errorPropertiesMap.get("status").toString());
+        return ServerResponse.status(status)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(errorPropertiesMap));
+    }
+}
---
+++ stu_spring/webflux/auth/src/main/java/com/webflux/auth/global/error/exception/GlobalException.java
@@ -0,0 +1,14 @@
+package com.webflux.auth.global.error.exception;
+
+import lombok.Getter;
+
+@Getter
+public class GlobalException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public GlobalException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
