a2ec16cb3016f6ff40f98c3136cd9683bd6c2032
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-06-14 11:03:16 +0900

    :gear: :: (#6) 로그인/회원가입을 위한 프로젝트 Initial Commit

---
+++ stu_spring/webflux/auth/src/test/java/com/webflux/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webflux.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
