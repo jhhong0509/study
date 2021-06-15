cb66cb9d8097fb18559c264c416971e289caa9fb
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-06-15 14:03:46 +0900

    ⚡️ :: (#6) User 엔티티 수정

--- stu_spring/webflux/auth/src/main/java/com/webflux/auth/domain/user/entity/User.java
+++ stu_spring/webflux/auth/src/main/java/com/webflux/auth/domain/user/entity/User.java
@@ -1,18 +1,25 @@
 package com.webflux.auth.domain.user.entity;
 
+import com.mongodb.lang.NonNull;
 import lombok.Builder;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Builder
+@Getter
 @Document
 public class User {
 
     @Id
-    private String _id;
+    private final String id;
 
-    private String email;
+    @NonNull
+    @Indexed(unique = true)
+    private final String email;
 
-    private String password;
+    @NonNull
+    private final String password;
 
 }
