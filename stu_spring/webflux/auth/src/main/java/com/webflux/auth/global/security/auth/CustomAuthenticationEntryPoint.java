a35e3226758bfcca0868cbecbe07754e731946bf
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-06-16 10:34:15 +0900

    :bug: :: (#6) 401 오류시 반환 body 추가, Token Validate에서 RefreshToken임을 검증

--- stu_spring/webflux/auth/src/main/java/com/webflux/auth/global/security/auth/CustomAuthenticationEntryPoint.java
+++ stu_spring/webflux/auth/src/main/java/com/webflux/auth/global/security/auth/CustomAuthenticationEntryPoint.java
@@ -1,19 +1,27 @@
-//package com.webflux.auth.global.security.auth;
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.MediaType;
-//import org.springframework.security.core.AuthenticationException;
-//import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.server.ServerWebExchange;
-//import reactor.core.publisher.Mono;
-//
-//@Component
-//public class CustomAuthenticationEntryPoint implements ServerAuthenticationEntryPoint {
-//    @Override
-//    public Mono<Void> commence(ServerWebExchange exchange, AuthenticationException ex) {
-//        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-//        exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
-//        return exchange
-//    }
-//}
+package com.webflux.auth.global.security.auth;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Component
+public class CustomAuthenticationEntryPoint implements ServerAuthenticationEntryPoint {
+    @Override
+    public Mono<Void> commence(ServerWebExchange exchange, AuthenticationException ex) {
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
+        
+        String str = "{\n" +
+                "  \"status\": 401,\n" +
+                "  \"message\": \"Not authenticated.\"\n" +
+                "}";
+
+        var buffer = exchange.getResponse().bufferFactory().wrap(str.getBytes());
+        return exchange.getResponse().writeWith(Flux.just(buffer));
+    }
+}
