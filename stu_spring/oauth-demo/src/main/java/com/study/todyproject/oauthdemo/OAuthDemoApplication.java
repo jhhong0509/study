138dca58373a81815f1578975a28e64f296edff7
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-08-25 08:36:31 +0900

    ⚡️ :: Application.java

---
+++ stu_spring/oauth-demo/src/main/java/com/study/todyproject/oauthdemo/OAuthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.study.todyproject.oauthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthDemoApplication.class, args);
+    }
+
+}
