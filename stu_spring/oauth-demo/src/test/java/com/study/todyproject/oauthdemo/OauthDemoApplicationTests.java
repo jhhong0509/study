10df9a73350e580ef60839dc68e1e595f4e804d7
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-08-25 08:32:12 +0900

    ⚡️ :: 테스트 추가

---
+++ stu_spring/oauth-demo/src/test/java/com/study/todyproject/oauthdemo/OauthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.todyproject.oauthdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
