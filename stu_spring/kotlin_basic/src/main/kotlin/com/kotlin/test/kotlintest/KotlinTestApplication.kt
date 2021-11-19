42c901f54a44d64af28a5ccf78d38f7663f0a5a3
jhhong0509 <jhhong0509@naver.com>
2021-11-19 11:24:33 +0900

    ⚡️ :: (#29) Property 추가

--- stu_spring/kotlin_basic/src/main/kotlin/com/kotlin/test/kotlintest/KotlinTestApplication.kt
+++ stu_spring/kotlin_basic/src/main/kotlin/com/kotlin/test/kotlintest/KotlinTestApplication.kt
@@ -1,8 +1,10 @@
 package com.kotlin.test.kotlintest
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class KotlinTestApplication
 
