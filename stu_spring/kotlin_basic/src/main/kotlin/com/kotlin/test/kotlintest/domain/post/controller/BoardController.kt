b7a27c26cd9647117d840304b8f1ba1b6008b8e3
jhhong0509 <jhhong0509@naver.com>
2021-11-19 16:15:08 +0900

    ⚡️ :: (#29) 게시글 한 개 조회 추가

--- stu_spring/kotlin_basic/src/main/kotlin/com/kotlin/test/kotlintest/domain/post/controller/BoardController.kt
+++ stu_spring/kotlin_basic/src/main/kotlin/com/kotlin/test/kotlintest/domain/post/controller/BoardController.kt
@@ -1,10 +1,9 @@
 package com.kotlin.test.kotlintest.domain.post.controller
 
 import com.kotlin.test.kotlintest.domain.post.payload.request.CreateBoardRequest
+import com.kotlin.test.kotlintest.domain.post.payload.response.BoardContentResponse
 import com.kotlin.test.kotlintest.domain.post.service.BoardService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 @RestController
@@ -15,6 +14,11 @@ class BoardController(
     @PostMapping("/board")
     fun saveBoard(@RequestBody @Valid boardRequest: CreateBoardRequest) {
         boardService.saveBoard(boardRequest)
+    }
+
+    @GetMapping("/board/{id}")
+    fun saveBoard(@PathVariable id: Long) : BoardContentResponse{
+        return boardService.getOneBoard(id)
     }
 
 }
--- stu_spring/kotlin_basic/src/main/kotlin/com/kotlin/test/kotlintest/domain/post/service/BoardService.kt
+++ stu_spring/kotlin_basic/src/main/kotlin/com/kotlin/test/kotlintest/domain/post/service/BoardService.kt
@@ -2,20 +2,36 @@ package com.kotlin.test.kotlintest.domain.post.service
 
 import com.kotlin.test.kotlintest.domain.post.entity.Board
 import com.kotlin.test.kotlintest.domain.post.entity.BoardRepository
+import com.kotlin.test.kotlintest.domain.post.exceptions.BoardNotFoundException
 import com.kotlin.test.kotlintest.domain.post.payload.request.CreateBoardRequest
+import com.kotlin.test.kotlintest.domain.post.payload.response.BoardContentResponse
 import com.kotlin.test.kotlintest.domain.user.entity.User
 import com.kotlin.test.kotlintest.domain.user.entity.UserFacade
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 class BoardService(
     private val boardRepository: BoardRepository,
     private val userFacade: UserFacade
-){
+) {
     fun saveBoard(boardRequest: CreateBoardRequest) {
         val user = userFacade.getCurrentUser()
         val board = buildBoard(boardRequest, user)
         boardRepository.save(board)
+    }
+
+    fun getOneBoard(id: Long) : BoardContentResponse{
+        val userBoard = boardRepository.findByIdOrNull(id) ?: throw BoardNotFoundException.EXCEPTION
+        return userBoard.let { board ->
+            BoardContentResponse(
+                title = board.title,
+                content = board.content,
+                createdAt = board.createdAt ?: LocalDateTime.now(),
+                writerName = board.user.name
+            )
+        }
     }
 
     private fun buildBoard(boardRequest: CreateBoardRequest, writer: User): Board {
