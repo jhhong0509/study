508396027e4adca53dc86ee6c74da55504bf5dd6
jhhong0509 <jhhong0509@naver.com>
2021-11-19 14:55:05 +0900

    ⚡️ :: (#29) 필요없는 바디 삭제 / 비밀번호 불일치 예외 추가

--- stu_spring/kotlin_basic/src/main/kotlin/com/kotlin/test/kotlintest/global/exception/GlobalException.kt
+++ stu_spring/kotlin_basic/src/main/kotlin/com/kotlin/test/kotlintest/global/exception/GlobalException.kt
@@ -2,5 +2,4 @@ package com.kotlin.test.kotlintest.global.exception
 
 import com.kotlin.test.kotlintest.global.exception.enums.ErrorCode
 
-open class GlobalException(val code: ErrorCode) : RuntimeException() {
-}
+open class GlobalException(val code: ErrorCode) : RuntimeException()
